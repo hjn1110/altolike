ebd57076ced65c4bf9c382b051235b571e28e577
hjn1110 <hjn1110@163.com>
2021-10-06 23:06:05 +0800

    feat: altolike static light rendering
    Alto's adventure-like light rendering depends:
    - 2d light (point light & global light)
    - Bloom (glow of the sun)
    - 2d grayscale sprite ( landscape & vision fog )
    
    ---
    
    2d light depends:
    Package
    - Universal RP
    Assets
    - Universal RP Asset ( with HDR setting enabled )
    - Renderer List: set to 2D Renderer Data
    
    Bloom depends:
    Package
    - Visual Effect Graph
    Asset
    - Volume Profile
    - Camera ( with Post Procecssing setting enabled )
    Shader
    ShaderController
    - a c# script used to cooperate with the shader to pass the color generated by the shader to the material

---
+++ Assets/Scripts/GlowController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowController : MonoBehaviour
+{
+
+    [ColorUsage(false, true)]
+    public Color glowColor;
+
+    [HideInInspector]
+    public Material glowMa;
+     
+    private void Start()
+    {
+        initGlowMa();
+        initBloom();
+    }
+
+    public virtual void initGlowMa()
+    {
+        glowMa = GetComponent<Renderer>().material;
+
+    }
+
+    public virtual void initBloom()
+    {
+        glowMa.SetColor("Color_9EE04840", glowColor);
+    }
+
+
+}
